f1df808649800b182a83bf017ee68add79528506
Angie-n <68796619+Angie-n@users.noreply.github.com>
2024-02-20 02:05:07 -0800

    Style changes

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -9,7 +9,7 @@ export default function About() {
             <h2>WHO I AM</h2>
             <div id='about-info'>
                 <img src={profilePic.src} alt="Photo of me"/>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at felis tincidunt, maximus justo ut, hendrerit lacus. Vestibulum posuere sodales auctor. Aliquam id risus ut lorem tincidunt facilisis. Vestibulum vestibulum pharetra imperdiet. Donec vulputate eleifend risus, congue egestas dui tempus sed. In eget commodo turpis. Integer et enim enim. Suspendisse euismod elit eros, in lacinia lectus posuere quis. Sed lobortis sapien neque, nec tempus lorem finibus at. Duis venenatis lorem ut sem varius, ut convallis ipsum mollis.</p>
+                <p>Hi there, my name is Angie Nguyen and I’m currently a UCSD student majoring in computer science. I believe the best way to learn is through getting hands-on experience, and so I’m always eager to work on new projects. While I have the self-drive to learn on my own and follow through on solo projects, I am always looking for opportunities to get involved and have joined teams on campus.</p>
             </div>
         </div>
     )
