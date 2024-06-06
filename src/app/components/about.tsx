035ac8601c81049a3c6c82392976b2cabfc8c28d
Angie-n <68796619+Angie-n@users.noreply.github.com>
2024-06-06 12:10:28 -0700

    Edited introduction

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,15 +1,13 @@
 'use client'
 
 import "../styles/about.css";
-import profilePic from '../images/profile.png'
  
 export default function About() {
     return (
         <div id='about'>
-            <h2>WHO I AM</h2>
+            <h2>AN INTRODUCTION</h2>
             <div id='about-info'>
-                <img src={profilePic.src} alt="Photo of me"/>
-                <p>Hi there, my name is Angie Nguyen and I’m currently a UCSD student majoring in computer science. I believe the best way to learn is through getting hands-on experience, and so I’m always eager to work on new projects. While I have the self-drive to learn on my own and follow through on solo projects, I am always looking for opportunities to get involved and have joined teams on campus.</p>
+                <p>Hi there, my name is Angie Nguyen and I’m currently a UCSD student majoring in computer science. I believe the best way to learn is through getting hands-on experience, and so I’m always eager to work on new projects. While I have the self-drive to learn on my own and follow through on solo projects, I am always looking for opportunities to get involved. Feel free to contact me if you think I’d be a good fit for your project!</p>
             </div>
         </div>
     )
